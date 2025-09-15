542e7cbe16353aed556eff6e538b425188a52a89
刘治谨 <xsro@foxmail.com>
2025-09-15 21:49:59 +0800

    add section estimation

--- latex.control.adaptive.ctexbeamer/python/adaptive_parameter.py
+++ latex.control.adaptive.ctexbeamer/python/adaptive_parameter.py
@@ -1,40 +1,47 @@
+#%%
 from scipy.integrate import solve_ivp
 import numpy as np
 import matplotlib.pyplot as plt
 
 #%%
-def rhs(t,x):
+def w1(t):
+    return np.sin(t)
+
+def rhs(t,x,w_func):
     e=x[0]
     theta=x[1]
-    w=np.sin(t)
+    w=w_func(t)
     de=-e+theta*w
     dtheta=-e*w
     return np.array([de,dtheta])
 
-sol=solve_ivp(rhs,[0,40],[-1,0.4])
+t_eval=np.arange(0,40,0.01)
+sol=solve_ivp(rhs,[0,40],[-1,1.4],args=(w1,),dense_output=True,t_eval=t_eval)
 plt.figure(figsize=(6,3))
 plt.plot(sol.t,sol.y[0,:],label=r"$e$")
-plt.plot(sol.t,sol.y[1,:],label=r"$\theta$")
+plt.plot(sol.t,sol.y[1,:],"--",label=r"$\theta$")
+plt.plot(sol.t,np.vectorize(w1)(sol.t),"--",label=r"$w(t)$")
 plt.legend()
 plt.grid()
 plt.savefig("../figure/simple-pe.pdf")
 plt.show()
+
+
 # %%
-def rhs(t,x):
-    e=x[0]
-    theta=x[1]
-    if t<5:
-        w=1
+def w2(t):
+    if t<2*np.pi:
+        w=np.sin(t)
     else:
         w=0
-    de=-e+theta*w
-    dtheta=-e*w
-    return np.array([de,dtheta])
+    return w
 
-sol=solve_ivp(rhs,[0,40],[-1,0.4])
+
+t_eval=np.arange(0,40,0.01)
+sol=solve_ivp(rhs,[0,40],[-1,1.4],args=(w2,),dense_output=True,t_eval=t_eval)
 plt.figure(figsize=(6,3))
 plt.plot(sol.t,sol.y[0,:],label=r"$e$")
 plt.plot(sol.t,sol.y[1,:],label=r"$\theta$")
+plt.plot(sol.t,np.vectorize(w2)(sol.t),"--",label=r"$w(t)$")
 plt.legend()
 plt.grid()
 plt.savefig("../figure/simple-npe.pdf")
